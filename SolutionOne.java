661b60650c813d1f97986d2ae1615b3c8a62c901
Elif Avcı <88689894+elfavci@users.noreply.github.com>
2024-02-16 14:26:44 +0300

    Welcome to Java!
    Input Format:
    There is no input for this challenge.
    Output Format:
    You must print two lines of output:
    Print Hello, World. on the first line.
    Print Hello, Java. on the second line.
    Sample Output:
    Hello, World.
    Hello, Java.

---
+++ SolutionOne.java
@@ -0,0 +1,8 @@
+//Welcome to the world of Java
+public class SolutionOne {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+	}
+}

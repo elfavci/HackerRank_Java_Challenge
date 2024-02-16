e5ac9813345a62d09430910aea34f1759b2e2a3d
Elif Avcı <88689894+elfavci@users.noreply.github.com>
2024-02-16 14:28:55 +0300

    Java Stdin and Stdout I
    Challenge Two:In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line.

---
+++ SolutionTwo.java
@@ -0,0 +1,18 @@
+//Stdin and Stdout I
+import java.util.Scanner;
+public class SolutionTwo {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+        int a,b,c;
+        a=input.nextInt();
+        b=input.nextInt();
+        c=input.nextInt();
+        input.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+	}
+
+}
